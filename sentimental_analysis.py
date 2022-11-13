a39a7ca5e0fd14e46d18f6603cea64c5821b8d42
spevenexe <112666138+spevenexe@users.noreply.github.com>
2022-11-12 18:01:08 -0600

    Added sentimental_analysis, sample csv, and resulting csv

---
+++ sentimental_analysis.py
@@ -0,0 +1,21 @@
+from textblob import TextBlob
+import pandas as pd
+import csv
+
+#use textblob to get subjectivity... 
+def getSubjectivity(text):
+    return TextBlob(str(text)).sentiment.subjectivity
+#..and polarity
+def getPolarity(text):
+    return TextBlob(str(text)).sentiment.polarity
+
+reviews_data = pd.read_csv("Womens Clothing E-Commerce Reviews.csv")
+reviews_data.rename(columns={'Unnamed: 0':'Review ID'},inplace=True)
+
+#add new sentimentality columns
+reviews_data['Subjectivity'] = reviews_data['Review Text'].apply(getSubjectivity)
+reviews_data['Polarity'] = reviews_data['Review Text'].apply(getPolarity)
+
+#sort reviews_data, then write to external file
+sorted_data = reviews_data.sort_values(by=['Polarity'])
+sorted_data.to_csv('analyzed_data.csv')
